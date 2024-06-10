4ddc8c71031c69369a303b8aefe52ee2f5260e22
IvanPadillaSalgado <ivan.padilla@utel.edu.mx>
2024-06-10 12:26:58 -0600

    Se agrega el contexto de sqlite

---
+++ sistema-gestion-energia-servicio/ems-persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ems_persistencia;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=people.db");
+    }
+}
